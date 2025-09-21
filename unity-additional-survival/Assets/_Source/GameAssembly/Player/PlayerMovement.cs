4ff42e0a3aeda0b4b0ecc7cbabf8f31704e087fa
Алексей <ix.fenix@mail.ru>
2025-09-21 22:57:56 +0300

    tweak: player movement collision

--- unity-additional-survival/Assets/_Source/GameAssembly/Player/PlayerMovement.cs
+++ unity-additional-survival/Assets/_Source/GameAssembly/Player/PlayerMovement.cs
@@ -10,9 +10,11 @@ namespace Player
         [SerializeField] private float speed;
         
         [Inject] private InputSystem_Actions _input;
+        private Rigidbody2D _rb;
         
         private void Start()
         {
+            _rb = GetComponent<Rigidbody2D>();
             ObjectInjector.InjectObject(this);
         }
 
@@ -26,7 +28,8 @@ namespace Player
 
         private void Move(Vector2 movement)
         {
-            transform.position = (Vector2)transform.position + movement * (Time.deltaTime * speed);
+            var moveVector = (Vector2)transform.position + movement * speed;
+            _rb.MovePosition(moveVector);
         }
     }
 }
